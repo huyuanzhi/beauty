33d9bcd9e41e944c27031fb35f98b3ea8285d48f
darkshadow <86774059@qq.com>
2017-01-04 19:03:26 +0800

    httpclient

--- src/main/java/cn/darkshadow/api/controller/TopicController.java
+++ src/main/java/cn/darkshadow/api/controller/TopicController.java
@@ -1,5 +1,8 @@
 package cn.darkshadow.api.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @packageName: cn.darkshadow.api.controller
  * @description: XXXXXX
  */
-@RestController
+@Controller
 public class TopicController {
+
+    @RequestMapping(value = "/index")
+    private String index(Model model){
+        model.addAttribute("m","hello");
+        return "index";
+    }
 }
---
+++ src/main/java/test/HttpClientUtils.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.impl.DefaultBHttpClientConnectionFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+
+/**
+ * @author: huyuanzhi
+ * @version: 1.0
+ * @date: 2017/1/4
+ * @project: beauty
+ * @packageName: test
+ * @description: XXXXXX
+ */
+public class HttpClientUtils {
+
+    public static void main(String[] args) {
+
+    }
+}
