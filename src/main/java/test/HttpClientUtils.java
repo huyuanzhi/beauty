35658c6d64c6518f9d981e6ad496eb8ba57c0649
darkshadow <86774059@qq.com>
2017-01-04 19:17:45 +0800

    httpclient

--- src/main/java/test/HttpClientUtils.java
+++ src/main/java/test/HttpClientUtils.java
@@ -4,9 +4,16 @@ import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.DefaultBHttpClientConnectionFactory;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.cookie.BasicClientCookie;
+
+import java.util.List;
 
 /**
  * @author: huyuanzhi
@@ -19,6 +26,26 @@ import org.apache.http.impl.conn.PoolingClientConnectionManager;
 public class HttpClientUtils {
 
     public static void main(String[] args) {
+        CookieStore cookieStore = new BasicCookieStore();
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultCookieStore(cookieStore)
+                .build();
+        try {
 
-    }
+            HttpPost post = new HttpPost("http://www.baiduc.com");
+            BasicClientCookie cookie = new BasicClientCookie("name", "zhaoke");
+            cookie.setVersion(0);
+            cookie.setDomain("/pms/");   //设置范围
+            cookie.setPath("/");
+            cookieStore.addCookie(cookie);
+            httpClient.execute(post);//
+            List<Cookie> cookies = cookieStore.getCookies();
+            for (int i = 0; i < cookies.size(); i++) {
+                System.out.println("Local cookie: " + cookies.get(i));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+
+        }  }
 }
